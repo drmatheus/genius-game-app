6fbb1c4b3978f6d0d5bccebd032d497c519b7c6a
drmatheus <davilamatheus@outlook.com>
2023-07-14 13:06:54 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
+      <div className="bg-slate-700">teste</div>
+      <div className="bg-slate-700">teste</div>
+      <div className="bg-slate-700">teste</div>
+    </main>
+  );
+}
